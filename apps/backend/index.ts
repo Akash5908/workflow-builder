61850a4a101061d09aa7c69b7d64b67425760a7f
Akash5908 <venomkr020@gmail.com>
2026-02-02 11:50:58 +0530

    Deploy error solve

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -11,9 +11,20 @@ import { WebhookRouter } from "./routes/webhook/webhook.routes";
 
 const app = express();
 app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://workflow-builder-k0hc.onrender.com",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
